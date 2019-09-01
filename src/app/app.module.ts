afa298b6c83700d9d6d1a4874a7a8db251733cfa
Kellan Ryan <KellanRyan0707@gmail.com>
2019-09-01 11:06:04 -0500

    Changed user logic on blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule }      from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientXsrfModule } from '@angular/common/http';
-import axios from 'axios'
 
 
 import { AppRoutingModule } from './app-routing.module';
